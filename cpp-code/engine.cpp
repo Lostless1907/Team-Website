80b3a150af53ccbe5259cc8c8cb35c3466abd7c7
Lostless1907 <206261279+Lostless1907@users.noreply.github.com>
2026-01-12 20:02:50 +0530

    Update engine.cpp

--- cpp-code/engine.cpp
+++ cpp-code/engine.cpp
@@ -1,7 +1,7 @@
 #include <emscripten.h>
 #include "physics/Vec3.hpp"
 #include "physics/integrators.hpp"
-// #include "control/pid.hpp"  // Uncomment if you have this file
+#include "control/pid.hpp"  
 
 // Global state for the Inverse Matrix engine
 double altitude = 0.0;
@@ -16,7 +16,7 @@ extern "C" {
     
     EMSCRIPTEN_KEEPALIVE
     double step_physics(double dt, double thrust) {
-        // Simple physics: F = ma, a = F/m - g
+    
         const double mass = 1.0;  // kg
         const double g = 9.81;    // gravity
         
